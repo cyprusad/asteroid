c6812162ed22eb40c361e0d68eb5c81fc42cc283
Sai Warang <cyprusad@gmail.com>
2015-09-13 23:11:09 -0400

    reactively render all the db posts to client by find()ing them in the template handler

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,25 +1,5 @@
-var postsData = [
-  {
-    title: "I thought of giving a talk about this stuff.",
-    author: "Sai Warang",
-    url: "http://www.saiwarang.com"
-  },
-  {
-    title: "Meanwhile, elsewhere, people are tackling real problems.",
-    author: "Sai Warang",
-    url: "http://www.cnn.com"
-  },
-  {
-    title: "I am out of stuff to say. I am so pooped",
-    author: "Sai Warang",
-    url: "http://localhost:3000"
-  },
-  {
-    title: "We need some diversity of opinion here...",
-    author: "Sai Warang",
-    url: "http://twitter.com/cyprusad"
-  }
-];
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
